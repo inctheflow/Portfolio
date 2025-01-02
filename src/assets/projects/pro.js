514514c271edd84fe9099ae8f98fa5b0fa8e1a7d
Aditya Raj Poudel <poudel34@students.rowan.edu>
2025-01-01 22:28:51 -0500

    initial

---
+++ src/assets/projects/pro.js
@@ -0,0 +1,14 @@
+const dataProjects = [
+    {
+        "id": 1,
+        "name": "Personal Portfolio Website",
+        "image": "https://github.com/Abhi-arch246/abhi-portfolio/assets/45809360/74309bbe-1750-40ea-9f52-56d268523db8",
+        "desc": "Github link",
+        "link": "https://github.com/Abhi-arch246/abstore-ecom-paypal",
+        "status":"cursor-pointer"
+    },
+ 
+]
+
+export default dataProjects
+
